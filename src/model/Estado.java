00ff565e17ab591405cc27a693e34e8ad04616a2
Celso <celsodiehl@gmail.com>
2025-07-28 10:04:53 -0300

    Criado classe model Estado

---
+++ src/model/Estado.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estado")
+public class Estado implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 40)
+    private String nome;
+
+    @Column(length = 2)
+    private String uf;
+
+    @Column(length = 2)
+    private int ibge;
+
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades;
+
+    public Estado() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public int getIbge() {
+        return ibge;
+    }
+
+    public void setIbge(int ibge) {
+        this.ibge = ibge;
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+    
+    @Override
+    public String toString() {
+        return getUf();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estado other = (Estado) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+}
