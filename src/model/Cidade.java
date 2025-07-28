5dd59bb8f7b9747c8d8040c8e46e1921eaf7a5c9
Celso <celsodiehl@gmail.com>
2025-07-28 10:05:54 -0300

    Criado classe model Cidade

---
+++ src/model/Cidade.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cidade")
+public class Cidade implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "estado", nullable = false, referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_estado"))
+    private Estado estado;
+    
+    @Column(length = 80)
+    private String nome;
+    
+    @Column(length = 2)
+    private String uf;
+    
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "estado")
+//    private Estado estado;
+    
+    //
+    
+    //id serial NOT NULL,
+    //estado int NOT NULL default '00',
+    //uf varchar(4) NOT NULL default '',
+    //nome 
+    //
+
+    public Cidade() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 19 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cidade other = (Cidade) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+}
